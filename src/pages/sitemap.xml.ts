903cad8fe03ff9d21f3d06727dd4f71f20b86ab5
Barney Powell <9741234+BarneyPowell@users.noreply.github.com>
2026-07-08 22:19:59 +0100

    feat: add SEO metadata scaffolding

---
+++ src/pages/sitemap.xml.ts
@@ -0,0 +1,33 @@
+import { site } from "../data/site";
+
+const pages = [
+  {
+    path: "/",
+    changefreq: "monthly",
+    priority: "1.0",
+  },
+] as const;
+
+export function GET() {
+  const urls = pages
+    .map(({ path, changefreq, priority }) => {
+      const loc = new URL(path, site.url).toString();
+
+      return `  <url>
+    <loc>${loc}</loc>
+    <changefreq>${changefreq}</changefreq>
+    <priority>${priority}</priority>
+  </url>`;
+    })
+    .join("\n");
+
+  return new Response(`<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+${urls}
+</urlset>
+`, {
+    headers: {
+      "Content-Type": "application/xml; charset=utf-8",
+    },
+  });
+}
